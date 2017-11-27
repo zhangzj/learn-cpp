201d726b56aa8c02f6f98606e79c71a3eaaaf562
zhangzhoujian <zhangzhoujian@zhangzhoujian.com>
2017-11-27 22:59:29 +0800

    add tutorial for hello world

---
+++ tutorial.cpp
@@ -0,0 +1,12 @@
+// hello world for tutorial
+#include "TutorialConfig.h"
+#include <iostream>
+int main (int argc, char *argv[])
+{
+ 
+    std::cout << "Hello, World!" << std::endl;
+ 
+    int a = 0;
+    std::cin >> a;
+    return 0;
+}
